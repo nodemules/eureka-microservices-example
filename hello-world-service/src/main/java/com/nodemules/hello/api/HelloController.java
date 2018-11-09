3dace2b0fc5393eb06d46db9d9861c6903a4c48e
brenthaertlein <brenthaertlein@gmail.com>
2018-11-08 23:42:30 -0500

    SOAP is a piece of shit

--- hello-world-service/src/main/java/com/nodemules/hello/api/HelloController.java
+++ hello-world-service/src/main/java/com/nodemules/hello/api/HelloController.java
@@ -1,13 +1,16 @@
 package com.nodemules.hello.api;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class HelloController {
 
   @GetMapping
   public String hello() {
+    log.info("hello");
     return "hello";
   }
 }
